1fe27fd34f3676004488ec5f748ffb705d9120f1
jjuguriKing <kyi108@naver.com>
2019-07-01 16:58:10 +0900

    Initial Commit

---
+++ MyGitTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyGitTest
+//
+//  Created by 503_23 on 01/07/2019.
+//  Copyright © 2019 kyi108. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
